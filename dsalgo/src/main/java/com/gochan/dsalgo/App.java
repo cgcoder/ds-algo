cc8924af7414c881cc7e064b8d744e5e2bb2aae2
gopichan <gopinath.chand@hotmail.com>
2019-02-25 06:08:19 -0800

    initial checkin

---
+++ dsalgo/src/main/java/com/gochan/dsalgo/App.java
@@ -0,0 +1,13 @@
+package com.gochan.dsalgo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
